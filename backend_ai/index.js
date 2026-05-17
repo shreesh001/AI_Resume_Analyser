0bdd8065a4175fc823c7afbae83825819d40879a
shreesh001 <shreeshpathak1@gmail.com>
2026-05-18 00:37:33 +0530

    axios and cors tested

--- backend_ai/index.js
+++ backend_ai/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 require('./conn');
+
 app.use(express.json());
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:5173'
+}));
 
 const userRoute = require('./Routes/user');
 const resumeRoute = require('./Routes/resume');
--- frontend_ai/src/component/Login/Login.jsx
+++ frontend_ai/src/component/Login/Login.jsx
@@ -8,49 +8,58 @@ import { useContext } from "react";
 import { AuthContext } from "../../utils/AuthContext";
 import { constant } from "firebase/firestore/pipelines";
 import { useNavigate } from "react-router-dom";
+import axios from "../../utils/axios";
 
 const Login = () => {
-  const navigate = useNavigate();
-  const { setIsAuthenticated, setUserInfo } = useContext(AuthContext);
-  const handlelogin=async()=>{
-    try{
-        const resut = await signInWithPopup(auth, provider);
-        const user = resut.user;
+    const navigate = useNavigate();
+    const { setIsAuthenticated, setUserInfo } = useContext(AuthContext);
+    const handlelogin = async () => {
+        try {
+            const resut = await signInWithPopup(auth, provider);
+            const user = resut.user;
 
-        const userData={
-            name: user.displayName,
-            email: user.email,
-            photoURL: user.photoURL
+            const userData = {
+                name: user.displayName,
+                email: user.email,
+                photoURL: user.photoURL
+            }
+
+            await axios.post('/api/user', userData).then((response) => {
+                setUserInfo(response.data.user);
+                setIsAuthenticated(true);
+
+                // Store user info in localStorage
+                localStorage.setItem("userInfo", JSON.stringify(response.data.user));
+                localStorage.setItem("islogin", true);
+                console.log(response.data);
+            })
+                .catch(err => {
+                    console.log(err);
+                    alert("Login failed. Please try again.");
+                })
+
+            navigate('/dashboard');
+        }
+        catch (err) {
+            alert("Login failed. Please try again.");
+            console.log(err);
         }
 
-        // Store user info in localStorage
-        localStorage.setItem("islogin", true);
-        localStorage.setItem("userInfo", JSON.stringify(userData));  
-        setIsAuthenticated(true);
-        setUserInfo(userData);
-
-        navigate('/dashboard');
     }
-    catch(err){
-        alert("Login failed. Please try again.");
-        console.log(err);
-    }
-    
-  }
-  return (
-    <div className={styles.Login}>
-        <div className={styles.loginCard}>
-            <div className={styles.loginCardTitle}>
-                <h1>Login</h1>
-                <VpnKeyIcon/>
-            </div>
+    return (
+        <div className={styles.Login}>
+            <div className={styles.loginCard}>
+                <div className={styles.loginCardTitle}>
+                    <h1>Login</h1>
+                    <VpnKeyIcon />
+                </div>
 
-            <div className={styles.googlebtn} onClick={handlelogin}>
-                <GoogleIcon sx={{fontSize: "20px", color:"red"}}/> Sign in with Google
+                <div className={styles.googlebtn} onClick={handlelogin}>
+                    <GoogleIcon sx={{ fontSize: "20px", color: "red" }} /> Sign in with Google
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Login
